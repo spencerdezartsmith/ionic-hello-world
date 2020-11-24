1570f7e39e91401b39a69d6cdc2518e66346983b
Spencer Dezart-Smith <spencerdezartsmith@gmail.com>
2020-11-24 15:28:39 +1100

    Set up basic hello world ionic app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { play } from 'ionicons/icons';
+import {
+  IonApp,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonToast,
+  IonToolbar,
+  IonTitle,
+} from '@ionic/react';
 
 function App() {
+  const [showToast, setShowToast] = useState(false);
+
   return (
-    <div>
-      <header>
-        <h1>My App</h1>
-      </header>
-      <main>
-        <p>Add some content here…</p>
-      </main>
-    </div>
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>My App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent class="ion-padding">
+        <IonToast
+          isOpen={showToast}
+          onDidDismiss={() => setShowToast(false)}
+          message="Your settings have been saved"
+          duration={800}
+          color="medium"
+        />
+        <IonButton
+          onClick={() => setShowToast(true)}
+          expand={true}
+          color="primary"
+        >
+          <IonIcon icon={play} slot="start" />
+          Click me
+        </IonButton>
+      </IonContent>
+    </IonApp>
   );
 }
 
